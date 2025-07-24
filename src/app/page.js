88dcb25acf8fae6b4d8d282adb4fbda77dcceb28
AzulaFire <jhornjr@gmail.com>
2025-07-24 16:48:16 +0900

    Fixed responsive viewport

--- src/app/page.js
+++ src/app/page.js
@@ -46,7 +46,7 @@ export default function Home() {
   };
 
   return (
-    <main className='p-4 max-w-4xl mx-auto'>
+    <main className='px-4 py-6 max-w-4xl mx-auto w-full'>
       <h1 className='text-3xl font-bold mb-6 text-white'>
         Japanese Vocabulary
       </h1>
